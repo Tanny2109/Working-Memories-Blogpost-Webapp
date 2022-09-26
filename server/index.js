2638e15c2132d50d8eda709f6839e7ce57b656a1
tanny2109 <tanmayss2109@gmail.com>
2022-09-26 20:31:11 +0530

    adding correct files

--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
 
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
@@ -14,11 +16,13 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://js_mastery:123123123@practice.jto9p.mongodb.net/test';
-const PORT = process.env.PORT|| 5000;
+//tutorial method of connecting
+const CONNECTION_URL = "mongodb+srv://tanmaysutar:hansipansi21@cluster0.6zida.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const PORT = process.env.PORT|| 1111;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
 mongoose.set('useFindAndModify', false);
+
